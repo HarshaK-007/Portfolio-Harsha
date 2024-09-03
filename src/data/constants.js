9c91d1bb1104d6a2018ef91d42dc91c159ab2987
vamsi <vamsibalivada25@gmail.com>
2024-09-03 20:22:44 +0530

    updated_resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HarshaK-007",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1whZZnChz1Ik_i6wkRrlPGlmRKykQcZEk/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/harsha-kancharana-945845226/",
   twitter: "https://x.com/HarshaK_007",
   facebook: "https://www.facebook.com/profile.php?id=100089847055520",
