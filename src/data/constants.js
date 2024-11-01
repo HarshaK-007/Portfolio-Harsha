31c904ff8d085b8f165e82a319576fc0517233a0
HarshaK-007 <harshakancharana2002@gmail.com>
2024-11-01 11:25:27 +0530

    changes1

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,15 +1,13 @@
 export const Bio = {
   name: "Harsha Kancharana",
   roles: [
-    "Frontend Developer",
-    "Programmer",
+    "Full Stack Developer",
+    "ML-AI enthusiast",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HarshaK-007",
-  resume:
-    "https://drive.google.com/file/d/1whZZnChz1Ik_i6wkRrlPGlmRKykQcZEk/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/harsha-kancharana-945845226/",
+  linkedin: "https://www.linkedin.com/in/harsha-kancharana",
   twitter: "https://x.com/HarshaK_007",
   facebook: "https://www.facebook.com/profile.php?id=100089847055520",
   instagram: "https://www.instagram.com/harsha.kancharana/"
@@ -38,18 +36,29 @@ export const skills = [
         image:
           "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
       },
-     
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
     ],
   },
   {
     title: "Backend",
     skills: [
-     
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
       {
         name: "Postgresql",
         image: "https://www.postgresql.org/media/img/about/press/elephant.png",
       },
-     
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
       {
         name: "Firebase",
         image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
@@ -64,7 +73,11 @@ export const skills = [
         image:
           "https://w7.pngwing.com/pngs/889/976/png-transparent-the-c-programming-language-computer-programming-programming-miscellaneous-blue-computer-thumbnail.png",
       },
-      
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
       {
         name: "C",
         image:
@@ -90,7 +103,11 @@ export const skills = [
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
       },
-     
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
     ],
   },
 ];
@@ -103,7 +120,7 @@ export const experiences = [
     role: "Web Development Intern",
     company: "Oasis Infobyte",
     date: "Sep 2023 - Oct 2023",
-    desc: "Created TO-DO list and Calculator app as a part of the internship",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
     skills: [
       "HTML",
       "CSS",
@@ -112,19 +129,17 @@ export const experiences = [
     doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-harsha.appspot.com/o/Cerificates%2FOasis_Intern.png?alt=media&token=ff2d0a37-0ce9-4675-92b6-e8d238e8604a",
   },
   {
-    id: 2,
-    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-harsha.appspot.com/o/Experiences%2Frinl.png?alt=media&token=8db990e0-b041-44d3-ac55-74a0b915ae7f",
-    role: "Junior Assistant Trainee",
-    company: "RINL Visakapatnam Steel Plant",
-    date: "May 2023 - June 2023",
-    desc: "I worked as Trainee in Electrical Repair Workshop (ERS). The main objective of ERS is to repair of any faulty Electrical Equipment in the plant. Thus the process of repairing is also called as REVERSE ENGINEERING PROCESS, it is under services department, which undertakes repair, rewinding and testing of all types of electrical equipment",
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-harsha.appspot.com/o/Experiences%2Focta.png?alt=media&token=f0931475-3b4b-49c3-b31e-3a972612c497",
+    role: "Frontend Development Intern",
+    company: "OctaNet Services Pvt Ltd",
+    date: "June 2024 - July 2024",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
     skills: [
-      "Teamwork",
-      "Leadership",
-      "Public Speaking",
-      "Problem Solving"
+      "HTML",
+      "CSS",
+      "JavaScript"
     ],
-    doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-harsha.appspot.com/o/Cerificates%2FRINL_Intern-1.png?alt=media&token=ff0f25e6-c6d9-460b-9463-fda9375cc28c",
   },
 ];
 
@@ -134,13 +149,10 @@ export const education = [
     img: "https://firebasestorage.googleapis.com/v0/b/portfolio-harsha.appspot.com/o/Education%2FNIT_Durgapur_Logo.png?alt=media&token=09c7c48a-1885-44aa-9408-4ad9c4ad69f2",
     school: "National Institue of Technology, Durgapur",
     date: "Dec 2021 - Apr 2025",
-    grade: "6.76 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Electrical Engineering at National Institute of Technology, Durgapur. I have completed 6 semesters and have a CGPA of 6.76. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
-    degree: "Bachelor of Technology - BTech, Electrical Engineering",
-  },
-  {
-    id: 1,
-    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-harsha.appspot.com/o/Education%2F12thCollege.png?alt=media&token=0f47d719-7786-4378-af87-7efb11c38d4a",
+    desc: "I am currently pursuing a Bachelor's degree at National Institute of Technology, Durgapur. I have completed 6 semesters and have a CGPA of 6.76. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech",
+  }
+  { id: 1,
     school: "Sri Chaitanya College, Vijayawada",
     date: "Jun 2019 - Apr 2021",
     grade: "97.6%",
